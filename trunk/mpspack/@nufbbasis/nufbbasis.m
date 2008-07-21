66977685b3880a9b1de526e4e4ad1e8065822f76
timo.betcke <timo.betcke@b951002f-fd50-0410-9c41-4d9c7e307aa9>
2008-07-21 16:30:21 +0000

    - nufbbasis is now functional

--- trunk/mpspack/@nufbbasis/nufbbasis.m
+++ trunk/mpspack/@nufbbasis/nufbbasis.m
@@ -1,3 +1,20 @@
+% NUFBBASIS - Create a basis of irregular Fourier-Bessel functions.
+% This objects creates a basis of irregular Fourier-Bessel functions of
+% degree nu*(1:N) (for Fourier-Bessel sine functions) or degree nu*(0:N)
+% (for Fourier-Bessel cosine functions). A nufbbasis object is created by
+%
+% nufb=nufbbasis(origin,nu,offset,branch,N,k,opts)
+% origin: The origin of the Fourier-Bessel functions
+% nu    : The fractional order of the basis
+% offset: The direction that corresponds to the angular variable theta=0
+% branch: Direction of the branch cut. It must not point into the domain
+% N     : Degree of the basis fct.
+% opts  : Optional arguments. Currently supported is
+%         - opts.type    : 's'  Create basis of Fourier-Bessel sine fct.
+%                          'c'  Create basis of Fourier-Bessel cosine fct.
+%                          'cs' Create basis of Fourier-Bessel cosine and
+%                               sine functions 
+
 classdef nufbbasis < handle & basis
 
     % Basis of irregular Fourier-Bessel functions.
@@ -9,53 +26,88 @@ classdef nufbbasis < handle & basis
         branch   % Complex angle that determines the branch cut
         offset   % Complex angle that determines the zero line of the Bessel
                  % fct.
-        realflag % Decide whether the basis is evaluated using real
-                 % sine/cos or complex exponentials
+        type     % 'c': Fourier-Bessel cosine basis
+                 % 's': Fourier-Bessel sine basis
+                 % 'cs': Fourier-Bessel cosine/sine basis (default)
     end
 
     methods
-        function regfb = regfbbasis(origin,N,realflag,k)
-            if nargin<4, k=NaN; end;
-            if nargin<3, realflag=1; end; % By default use real arithmetic
-            if nargin<2, N=20; end; % Default degree of FB fct.
-            if nargin<1, origin=0; end; % Default origin is zero
+        function nufb = nufbbasis(origin,nu,offset,branch,N,k,opts)
+            % 
+            if nargin<7, opts=[]; end;
+            if ~isfield(opts,'type'), opts.type='cs'; end;
+            if nargin<6, k=NaN; end;
+            if nargin<5, N=20; end;
 
-            regfb.k=k;
-            regfb.realflag=realflag;
-            regfb.N=N;
-            regfb.origin=origin;
+            nufb.k=k;
+            nufb.N=N;
+            nufb.branch=branch;
+            nufb.offset=offset;
+            nufb.nu=nu;
+            nufb.origin=origin;
+            if opts.type=='s', nufb.type='s'; nufb.Nf=N; end;
+            if opts.type=='c', nufb.type='c'; nufb.Nf=N+1; end;
+            if strcmp(opts.type,'cs'), nufb.type='cs'; nufb.Nf=2*N+1; end;
+
         end
-        function [A, An, Ax, Ay] = eval(regfb,pts)
+        function [A, A1, A2] = eval(nufb,pts)
 
             % Evaluates the basis at a given set of points
-            N=regfb.N; k=regfb.k;
+            N=nufb.N; k=nufb.k; nu=nufb.nu; origin=nufb.origin;
             np=length(pts.x); % Number of points
-            R=abs(pts.x-regfb.origin);
-            ang=angle(pts.x-regfb.origin);
-            bes=besselj(0:N,k*R);
-            c=cos(ang*(1:N));
-            s=sin(ang*(1:N));
-            A=[bes(:,1), bes(:,2:end).*c, bes(:,2:end).*s];
-            if nargout>1,
-                bespp=[bes(:,2:end),besselj(N+1,k*R)];
-                besr=(repmat(0:N,np,1).*bes-k*repmat(R,1,N+1).*bespp)./repmat(R,1,N+1);
-                bestc=-repmat(0:N,np,1).*bes.*sin(ang*(0:N));
-                bests=repmat(1:N,np,1).*bes(:,2:end).*cos(ang*(1:N));
-                Ar=[besr(:,1),besr(:,2:end).*c,besr(:,2:end).*s];
-                At=[bestc,bests];
-                Ax=repmat(cos(ang),1,2*N+1).*Ar-repmat(sin(ang),1,2*N+1).*At./repmat(R,1,2*N+1);
-                Ay=repmat(sin(ang),1,2*N+1).*Ar+repmat(cos(ang),1,2*N+1).*At./repmat(R,1,2*N+1);
-                nx1=real(pts.nx); nx2=imag(pts.nx);
-                An=repmat(nx1,1,2*N+1).*Ax+repmat(nx2,1,2*N+1).*Ay;
+            R=abs(pts.x-nufb.origin);
+            ang=angle(-(pts.x-nufb.origin)./nufb.branch);
+            offang=angle(-nufb.offset./nufb.branch);
+            if nufb.type=='s',
+                bes=besselj(nu*(0:N+1),k*R);
+                s=sin(nu*(ang-offang)*(1:N));
+                A=bes(:,2:end-1).*s;
+            elseif nufb.type=='c',
+                bes=besselj(nu*(-1:N+1),k*R);
+                c=cos(nu*(ang-offang)*(0:N));
+                A=bes(:,2:end-1).*c;
+            elseif strcmp(nufb.type,'cs'),
+                bes=besselj(nu*(-1:N+1),k*R);
+                s=sin(nu*(ang-offang)*(1:N));
+                c=cos(nu*(ang-offang)*(0:N));
+                A=[bes(:,2:end-1).*c,bes(:,3:end-1).*s];
             end
-            if ~regfb.realflag,
-                A=[A(:,1:N)-1i*A(:,N+1:end),A(:,0),A(:,1:N)+1i*A(:,N+1:end)];
-                if nargout>1,
-                    An=[An(:,1:N)-1i*An(:,N+1:end),An(:,0),An(:,1:N)+1i*An(:,N+1:end)];
-                    Ax=[Ax(:,1:N)-1i*Ax(:,N+1:end),Ax(:,0),Ax(:,1:N)+1i*Ax(:,N+1:end)];
-                    Ay=[Ay(:,1:N)-1i*Ay(:,N+1:end),Ay(:,0),Ay(:,1:N)+1i*Ay(:,N+1:end)];
+            if nargin>1,
+                if numel(find(R==0))>0,
+                    warning('Computing x/y or normal derivatives of regular Bessel functions at origin not implemented');
+                end
+                besr=k/2*(bes(:,1:end-2)-bes(:,3:end));
+                if nufb.type=='s',
+                    Ar=besr.*s;
+                    At=nu*repmat(1:N,np,1).*bes(:,2:end-1).*cos(nu*(ang-offang)*(1:N));
+                elseif nufb.type=='c',
+                    Ar=besr.*c;
+                    At=-nu*repmat(0:N,np,1).*bes(:,2:end-1).*sin(nu*(ang-offang)*(0:N));
+                elseif strcmp(nufb.type,'cs'),
+                    Ar=[besr.*c,besr(:,2:end).*s];
+                    At=[-nu*repmat(0:N,np,1).*bes(:,2:end-1).*sin(nu*(ang-offang)*(0:N)),...
+                        nu*repmat(1:N,np,1).*bes(:,3:end-1).*cos(nu*(ang-offang)*(1:N))];
+                end
+                ang0=angle(pts.x-origin); % Angle with respect to the original
+                % coordinate system shifted by
+                % origin
+                cc=repmat(cos(ang0),1,nufb.Nf); ss=repmat(sin(ang0),1,nufb.Nf);
+                RR=repmat(R,1,nufb.Nf);
+                if nargout==2,
+                    nx=repmat(real(pts.nx),1,nufb.Nf); ny=repmat(imag(pts.nx),1,nufb.Nf);
+                    A1=Ar.*(nx.*cc+ny.*ss)+At.*(ny.*cc-nx.*ss)./RR;
+                end
+                if nargout==3,
+                    A1=cc.*Ar-ss.*At./RR; A2=ss.*Ar+cc.*At./RR;
                 end
             end
         end
     end
 end
+
+            
+            
+            
+            
+            
+            
