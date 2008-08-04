5be32440ae90f21eb21b0e922061424753e6f69a
alex.h.barnett <alex.h.barnett@b951002f-fd50-0410-9c41-4d9c7e307aa9>
2008-08-04 20:39:03 +0000

    derivatives added to SLP + DLP layerpot

---
+++ trunk/mpspack/@layerpot/T.m
@@ -0,0 +1,84 @@
+function [A Sker Dker_noang] = T(k, s, t, o);
+% T - deriv of double layer potential discr matrix for density on a segment
+%
+%  T = T(k, s) where s is a segment returns the matrix discretization of
+%   the deriv-of-DLP integral operator with density on the segment,
+%      u(x) = int_s partial_n(x) partial_n(y) Phi(x,y) tau(y) ds(y)
+%   where Phi is the fundamental solution
+%      Phi(x,y) = (i/4) H_0^{(1)}(k|x-y|)    for k>0
+%                 (1/2pi) log(1/|x-y|)       for k=0
+%   for x each of the points on the self-same segment s. No jump relations
+%   are included. For self-interaction, no special quadrature is yet implemented
+%
+%  T = T(k, s, t) where t is a pointset (any object with fields t.x)
+%   uses s as the source segment as above, but target points in t.
+%   It is assumed t is not s.
+%
+%  T = T(k, s, [], opts) or T = T(k, s, t, opts) does the above two choices
+%   but with options struct opts including the following:
+%    opts.Sker = quad-unweighted kernel matrix of fund-sols (i/4)H_0
+%    opts.Dker_noang = quad-unweighted kernel matrix of fund-sols derivs
+%                      without the cosphi factors (prevents recomputation
+%                      of H1's), ie (ik/4)H_1
+%   Passing any of the above options stops it from being calculated internally.
+%   When k=0, the above options have no effect.
+%
+%  [T Sker Dker_noang] = T(...) also returns quad-unweighted
+%   kernel values matrices Sker, Dker_noang, when k>0 (empty for Laplace)
+%
+%  barnett 8/4/08, Tested by routine: testbasis.m, case 9 (non-self case only)
+if isempty(k) | isnan(k), error('T: k must be a number'); end
+if nargin<4, o = []; end
+if nargin<3, t = []; end
+if ~isfield(o, 'quad'), o.quad='m'; end; % default self periodic quadr
+self = isempty(t);               % self-interact: potentially sing kernel
+N = numel(s.x);                  % # src pts
+if self, t = s; end              % use source as target pts (handle copy)
+M = numel(t.x);                  % # target pts
+sp = s.speed/2/pi; % note: 2pi converts to speed wrt s in [0,2pi] @ src pt
+% compute all geometric parts of matrix...
+d = repmat(t.x, [1 N]) - repmat(s.x.', [M 1]); % C-# displacements mat
+r = abs(d);                                    % dist matrix R^{MxN}
+if self, r(diagind(r)) = 999; end % dummy nonzero diag values
+ny = repmat(s.nx.', [M 1]);      % identical rows given by src normals
+csry = conj(ny).*d;              % (cos phi + i sin phi).r
+nx = repmat(t.nx, [1 N]);        % identical cols given by target normals
+csrx = conj(nx).*d;              % (cos th + i sin th).r
+clear nx ny d
+if k==0                          % Laplace; don't reuse since v. fast anyway
+  A = -real(csry.*csrx)./(r.^2.^2)/(2*pi); % (-1/2pi)cos(phi-th)/r^2
+  Sker = []; Dker_noang = [];
+else
+  cc = real(csry).*real(csrx) ./ (r.*r);      % cos phi cos th
+  cdor = real(csry.*csrx) ./ (r.*r.*r);   % cos(phi-th) / r
+  if ~isfield(o, 'Sker')
+    o.Sker = utils.fundsol(r, k);           % Phi
+  end
+  if ~isfield(o, 'Dker_noang')
+    [A o.Dker_noang] = utils.fundsol_deriv(r, -cdor, k); % compute n-deriv Phi
+  else
+    [A o.Dker_noang] = utils.fundsol_deriv(r, -cdor, k, o.Dker_noang);
+  end
+  % put all this together to compute the kernel value matrix...
+  A = A + k^2 * cc .* o.Sker;
+  Sker = o.Sker; Dker_noang = o.Dker_noang; % pass out some calcs for later use
+end
+
+if self % ........... source curve = target curve; can be singular kernel
+  
+  if s.qtype=='p' & o.quad=='k' % Kapur-Rokhlin (ignores diagonal value)
+    [s w] = quadr.kapurtrap(N+1, o.ord);  % Zydrunas-supplied K-R weights
+    w = 2*pi * w;                 % change interval from [0,1) to [0,2pi)
+    A = circulant(w(1:end-1)) .* A .* repmat(sp.', [M 1]); % speed
+  
+  else       % ------ self-interacts, but no special quadr, just use seg's
+    % Use the crude approximation of kappa for diag, usual s.w off-diag...
+    A = A .* repmat(s.w, [M 1]);  % use segment usual quadrature weights
+    fprintf('warning: using T crude self-quadr, will be awful (no diag)!\n')
+    A(diagind(A)) = 0;
+  end
+  
+else % ............................ distant target curve, so smooth kernel
+  
+  A = A .* repmat(s.w, [M 1]);       % use segment quadrature weights
+end
--- trunk/mpspack/testbasis.m
+++ trunk/mpspack/testbasis.m
@@ -7,14 +7,15 @@
 %           - include GSL test cases (need to compile on Alex's machine)
 
 clear all classes
-verb = 0;                            % use verb=0, N=50, for more of a test.
+verb = 2;                            % use verb=0, N=50, for more of a test
+err = 1;                             % false, show vals / true, show errors
 dx = 0.01; g = -1:dx:1;              % plotting region
 [xx yy] = meshgrid(g, g);
 M = numel(xx);
 p = pointset([xx(:) + 1i*yy(:)], ones(size(xx(:)))); % set up n for x-derivs
 k = 10;
 
-for type = 1:7
+for type = 1:10
   switch type
    case {1,2,3}             % ................. Reg FB: slow real/cmplx, fast
     N = 10;               
@@ -36,6 +37,13 @@ for type = 1:7
     b = mfsbasis(@(t) exp(1i*t), -0.4, N, k, opts);  % tau keeps them outside
     c = 1.0; js = 1:b.Nf;
     fprintf('evaluating MFS basis... real=%d\n', opts.real)
+   
+   case {8,9,10}            % ................. SLP, DLP, SLP+DLP
+    N = 10; s = segment(N, [-1.5+.5i, .5i]);
+    lp = 'S'; if type==9, lp = 'D'; elseif type==10, lp = [1 1i]; end
+    b = layerpot(s, lp, k); if type==10, lp = 'SLP+D'; end % hack for text
+    c = 0.25; js = b.Nf;
+    fprintf('evaluating {S,D}LP basis... %sLP\n', lp)
   end
   tic; [A Ax Ay] = b.eval(p); t=toc;
   fprintf('\t%d evals (w/ x,y derivs) in %.2g secs = %.2g us per eval\n',...
@@ -49,17 +57,30 @@ for type = 1:7
   if verb
     showfields(g, g, u, c, sprintf('type %d: u_j', type));         % values
     %showfields(g, g, ux, c*k, sprintf('type %d: du_j/dx', type)); % x-derivs
+    %showfields(g, g, uy, c*k, sprintf('type %d: du_j/dy', type)); % y-derivs
   end
   % errors in x-derivs...   plots should have no values larger than colorscale
-  unje = (u(:,3:end,:)-u(:,1:end-2,:))/(2*dx) - ux(:,2:end-1,:); % FD error
+  if err
+    unje = (u(:,3:end,:)-u(:,1:end-2,:))/(2*dx) - ux(:,2:end-1,:); % FD error
+  else
+    unje = (u(:,3:end,:)-u(:,1:end-2,:))/(2*dx);              % FD value
+    %showfields(g, g(2:end-1), unje, c*k, ...
+    %           sprintf('type %d: FD val du_j/dx', type));
+  end
   fprintf('\tmax err in u_x from FD calc = %g\n', max(abs(unje(:))))
-  if verb, showfields(g, g(2:end-1), unje, c*(k*dx)^2, ...
+  if verb>1, showfields(g, g(2:end-1), unje, c*(k*dx)^2, ...
                       sprintf('type %d: FD err in du_j/dx', type));
   end
   % errors in y-derivs...   plots should have no values larger than colorscale
-  unje = (u(3:end,:,:)-u(1:end-2,:,:))/(2*dx) - uy(2:end-1,:,:); % FD error
+  if err
+    unje = (u(3:end,:,:)-u(1:end-2,:,:))/(2*dx) - uy(2:end-1,:,:); % FD error
+  else
+    unje = (u(3:end,:,:)-u(1:end-2,:,:))/(2*dx);                 % FD value
+    showfields(g(2:end-1), g, unje, c*k, ...
+                      sprintf('type %d: FD val du_j/dy', type));
+  end
   fprintf('\tmax err in u_y from FD calc = %g\n', max(abs(unje(:))))
-  if verb, showfields(g(2:end-1), g, unje, c*(k*dx)^2, ...
+  if verb>1, showfields(g(2:end-1), g, unje, c*(k*dx)^2, ...
                       sprintf('type %d: FD err in du_j/dy', type));
   end
 end
