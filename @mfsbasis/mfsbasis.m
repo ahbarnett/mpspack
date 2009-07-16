15c2282a338f9d850be2f297f56fbe56c319dd26
alex.h.barnett <alex.h.barnett@b951002f-fd50-0410-9c41-4d9c7e307aa9>
2009-07-16 14:49:11 +0000

    fixed regfbbasis at origin, doubled its speed, added derivs for k=0; removed periodic stuff from basis.m

--- @mfsbasis/mfsbasis.m
+++ @mfsbasis/mfsbasis.m
@@ -1,29 +1,35 @@
 classdef mfsbasis < handle & basis
 % MFSBASIS - create a fundamental solutions (MFS) basis set
 %
-%  b = MFSBASIS(Z, tau, N, k, opts) creates an MFS basis using charge points
+%  b = MFSBASIS(p, [], [], opts) where p is a pointset charge points
+%   allows the user to choose the charge points, from which N is determined.
+%
+%  b = MFSBASIS(y, [], [], opts) where y is a row vector of charge points
+%   allows the user to choose the charge points, from which N is determined.
+%
+%  b = MFSBASIS(Z, tau, N, opts) creates an MFS basis using charge points
 %   at Z(t + i.tau) where t are equally distributed in [0,2pi], Z is a given
 %   2pi-periodic analytic function, and tau is the distance off the curve.
 %   N is the number of charge points, and k is wavenumber. opts may contain:
 %   opts.real: if true use Y_0, otherwise use H_0, as fundamental solution.
 %
-%  b = MFSBASIS(y, [], [], k, opts) where y is a row vector of charge points
-%   allows the user to choose the charge points, from which N is determined.
-%
 %  b = MFSBASIS() creates MFS basis without any charge pts.
 %
-%  There should be other input formats for MFSBASIS!
+% Issues/notes:
+%  * There should be other input formats for MFSBASIS!
+%  * other location methods, inferface to mfsloc.m code from analytic_mfs
+%  * Fully implement double and combined potential
+%  * document opts.fast
 %
-% To do: other location methods, inferface to mfsloc.m code from analytic_mfs
-% Fully implement double and combined potential
+% Also see: LAYERPOT
   properties
     Z                     % function handle for charge curve
     tau                   % imaginary distance
     t                     % row vec of parameter values in [0,2pi]
     y                     % row vec of charge points (C-#s)
     realflag              % if true, use real part only, ie Y_0
-    fast                  % Hankel evaluation method (0 matlab, 1,2, faster)
-    eta                   % If eta=inf (default) then use single layer potential 
+    fast                  % Hankel evaluation method (0=matlab; 1,2=faster)
+    eta                   % If eta=inf (default) use single layer potential 
                           % MFS basis. For eta\neq inf
                           % use linear combination of double and single
                           % layer potential MFS basis
@@ -31,31 +37,35 @@ classdef mfsbasis < handle & basis
   end
   
   methods
-    function b = mfsbasis(Z, tau, N, k, opts) % ............... constructor
-      if nargin<5, opts = []; end
+    function b = mfsbasis(Z, tau, N, opts) % ............... constructor
+      if nargin<4, opts = []; end
       if ~isfield(opts, 'real'), opts.real = 0; end
       b.realflag = opts.real;
       if ~isfield(opts, 'fast'), opts.fast = 2; end
-      if ~isfield(opts,'eta'), opts.eta=inf; end
-      if opts.fast==2 & exist('@utils/greengardrokhlinhank106.mexglx')~=3
+      if ~isfield(opts, 'eta'), opts.eta = inf; end
+      if opts.fast==2 && exist('@utils/greengardrokhlinhank106.mexglx')~=3
         opts.fast = 1;               % downgrade the speed if 106 not available
       end
       b.fast = opts.fast;
       b.eta  = opts.eta;
-      if (b.fast>0 & b.eta<inf), 
+      if (b.fast>0 && b.eta<inf), 
           error('MFSERROR','Fast evaluation of combined basis not implemented');
       end
       if ~isempty(k), b.k = k; end
       if isnumeric(Z)                         % Z contains y list of MFS pts
         N = numel(Z);
         b.y = reshape(Z, [1 N]);
-        b.N = N; b.Nf = N;
+        b.N = N;
       elseif ~isempty(Z)                      % Z is a function handle
-        b.N = N; b.Nf = N;
+        b.N = N;
         b.Z = Z; b.tau = tau;
         b.t = 2*pi*(1:N)/N;                   % create row vec
         b.y = Z(b.t + 1i*tau);
       end
+    end
+    
+    function Nf = Nf(b)
+      Nf = b.N;
     end
     
     function [A B C] = eval(b, p, opts)
--- test/testbasis.m
+++ test/testbasis.m
@@ -1,32 +1,36 @@
-% test program for all basis types: reg FB (incl fast), real PWs, ...
+% Test program for all basis types: reg FB (incl fast), real PWs, ...
 % barnett 7/10/08, MFS added 7/20/08, fast hankel and bessel 9/5/08
 % adapted from ~/bdry/inclus/test_evalbasis.m
+% Now creates a `whole plane' domain for each basis to affect.
 %
 % To Do:
-% * reg FB: - fix |x|=0 NaN problem, same for nu FB.
+% * fix |x|=0 problem for nufbbasis.
+% * make the below test eval nargout=2 and =3. (switch by hand line 55)
 
-clear classes
+clear all classes
 verb = 2;                            % use verb=0, N=50, for more of a test
 err = 1;                             % false, show vals / true, show errors
-dx = 0.02; g = -1:dx:1;              % plotting region, -1:.01:1 default
+dx = 0.01; g = -1:dx:1;              % plotting region, -1:.01:1 default
 [xx yy] = meshgrid(g, g);
 M = numel(xx); o = [];
 p = pointset([xx(:) + 1i*yy(:)], ones(size(xx(:)))); % set up n for x-derivs
-k = 10;
+k = 0;                              % wavenumber for tests (may be 0)
+d = domain(); d.k = k;               % create R^2 domain
 
-for type = 1:10
+for type = 1:3
   switch type
    case {1,2,3}             % ................. Reg FB: real/cmplx, rescl
-    N = 10;               
-    opts.real = (type~=2); opts.rescale_rad = 1.0*(type==3); opts.usegsl = 0;
-    b = regfbbasis(0, N, k, opts);
+    N = 10;
+    opts.real = (type~=2); opts.rescale_rad = 1.5*(type==3); opts.usegsl = 0;
+    b = regfbbasis(0, N, opts); b.doms = d;  % attach basis to R^2 domain
     c = 0.5;                 % set caxis scale for this basis type
     js = 1:b.Nf;             % indices to plot, here all of them
-    fprintf('evaluating Reg FB basis... real=%d, resc=%d\n', opts.real, opts.rescale_rad)
+    fprintf('evaluating Reg FB basis... real=%d, resc=%g\n', opts.real, ...
+            opts.rescale_rad)
    case {4,5}              % ................. real plane waves: real/cmplx
     N = 10;
     opts.real = (type==4);
-    b = rpwbasis(N, k, opts);
+    b = rpwbasis(N, opts); b.doms = d;
     c = 2.0; js = 1:b.Nf;
     fprintf('evaluating real plane wave basis... real=%d\n', opts.real)
    
@@ -34,19 +38,23 @@ for type = 1:10
     N = 10;
     opts.real = (type==6);
     opts.fast = 2;       % about 10x faster than o.fast=0 (matlab hankel)!
-    b = mfsbasis(@(t) exp(1i*t), -0.4, N, k, opts);  % tau keeps them outside
-    c = 0.2; js = 1:b.Nf;
+    b = mfsbasis(@(t) exp(1i*t), -0.4, N, opts);  % tau keeps them outside
+    b.doms = d; c = 0.2; js = 1:b.Nf;
     fprintf('evaluating MFS basis... real=%d\n', opts.real)
    
    case {8,9,10}            % ................. SLP, DLP, SLP+DLP
     N = 10; s = segment(N, [-1.5+.5i, .5i]); c = 0.03;
     lp = 'S'; if type==9, lp = 'D'; c=c*k; elseif type==10, lp = [1 1i]; end
     o.fast = 2;       % about 5x faster than o.fast=0 (matlab hankel)!
-    b = layerpot(s, lp, k, o); if type==10, lp = 'SLP+D'; end % hack for text
-    js = 7; %b.Nf; % 1 is off the region, b.Nf is in the region
+    b = layerpot(s, lp, o); if type==10, lp = 'SLP+D'; end % hack for text
+    b.doms = d; js = 7; %b.Nf; % 1 is off the region, b.Nf is in the region
     fprintf('evaluating {S,D}LP basis... %sLP\n', lp), o.close = 0.4;
   end
-  tic; [A Ax Ay] = b.eval(p, o); t=toc;
+  
+  tic; [A Ax Ay] = b.eval(p, o); t=toc;  % test nargout=3 case
+  %tic; p.nx = ones(size(xx(:))); [A Ax] = b.eval(p, o);  % or test nargout=2
+  %  p.nx = 1i*p.nx; [A Ay] = b.eval(p, o); t=toc;
+  
   fprintf('\t%d evals (w/ x,y derivs) in %.2g secs = %.2g us per eval\n',...
           numel(A), t, 1e6*t/numel(A))
   n = numel(js);
