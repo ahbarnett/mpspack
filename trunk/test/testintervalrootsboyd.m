e317764f8935fcae5a7514863b8b81f305a10d96
alex.h.barnett <alex.h.barnett@b951002f-fd50-0410-9c41-4d9c7e307aa9>
2010-08-13 04:42:23 +0000

    trunk: minor tweak to utils.intervalrootsboyd

--- trunk/test/testintervalrootsboyd.m
+++ trunk/test/testintervalrootsboyd.m
@@ -1,9 +1,19 @@
 % test Boyd spectral rootfinding
-% Barnett 7/28/2010
+% Barnett 7/28/2010, 8/13/10
 
 f = @(x) sin(pi*x);
-[x e y u] = utils.intervalrootsboyd(f, [0 10]); x, max(e)
+
+disp('high accuracy request...')
+[x e y u ier] = utils.intervalrootsboyd(f, [0 10]); x, max(e)
 figure; plot(y, u, '-'); hold on; plot(x, 0*x, 'rx'); title('intervalrootsboyd')
 
-[x e y u] = utils.intervalrootsboyd(f, [0 10], struct('Ftol',1e-6)); x, max(e)
+disp('low accuracy request...')
+[x e y u ier] = utils.intervalrootsboyd(f, [0 10], struct('Ftol',1e-6)); x, max(e)
 size(u)
+
+disp('taxing one...')
+[x e y u ier] = utils.intervalrootsboyd(f, [0 100]); x, max(e)
+% note that errors e are too pessimistic near endpoints of interval - why?
+
+disp('should fail gracefully...')
+[x e y u ier] = utils.intervalrootsboyd(f, [0 1e3]); ier
